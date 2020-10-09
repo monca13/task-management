41871671c4cfbb0b0c59a69e555fc2ce2d18a7e8
monica <monica@smartmobe.com>
2020-10-10 00:33:03 +0545

    Maintained role and permissions and Adminlte3

--- app/Modules/Users/Entities/User.php
+++ app/Modules/Users/Entities/User.php
@@ -3,30 +3,44 @@
 namespace App\Modules\Users\Entities;
 
 use App\Constants\DBTables;
-use Illuminate\Foundation\Auth\User as Auth;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
 /**
  * Class User
  * @package App\Modules\Users\Entities
  */
-class User extends Auth
+class User extends Authenticatable
 {
     use HasRoles;
     protected $table = DBTables::USERS;
 
     /**
+     * The attributes that are mass assignable.
+     *
      * @var array
      */
     protected $fillable = [
-        'username',
-        'password',
-        'mobile_number',
-        'otp',
-        'otp_generated_at',
-        'status',
-        'notification_enabled',
-        'external_id',
-        'country_code',
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $guard_name = 'web';
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
